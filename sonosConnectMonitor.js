1df3a5cb2f0ada5072f555e6a41033f3fadc32e7
Dennis Madsen <dennis@hrmadsen.dk>
2015-03-29 23:42:28 +0200

    SonosObserver module added

--- sonosConnectMonitor.js
+++ sonosConnectMonitor.js
@@ -1,5 +1,7 @@
-var sonos = require('sonos');
+var sonosObserver = require('./lib/sonosObserver');
 var denon = require('./lib/denon');
+
+var sonosObserver = new sonosObserver();
 
 var avr = new denon({
 	host: '10.0.1.14'
@@ -7,29 +9,14 @@ var avr = new denon({
 
 avr.connect();
 
-var searchModel = "ZPS1"; //TODO: Change to Sonos Connect Model UUID
+sonosObserver.observeByName("Kitchen");
 
-var sonosDevice = null;
-var lastState = "";
-
-console.log('Searching for Sonos Connect on network');
-
-sonos.search(function(device, model) {
-	
-	if (model==searchModel) {
-		
-		device.getZoneAttrs(function(err, attrs) {
-			var name = attrs['CurrentZoneName'];
-			
-			console.log("Found Sonos Connect named \""+name+"\"");
-			sonosDevice = device;
-			query_getSonosState();			
-		});
-	}
+sonosObserver.on('DeviceAvailable', function(device) {
+	console.log('Found Sonos Connect');
 });
 
-function handleSonosConnectStarted() {
-	console.log("Sonos Connect started");
+sonosObserver.on('Started', function(device) {
+	console.log('Sonos Connect started');
 	
 	avr.powerOn(function () {
 		avr.setSourceToAUX(function() {
@@ -38,39 +25,12 @@ function handleSonosConnectStarted() {
 			});
 		});
 	});
-}
+});
 
-function handleSonosConnectStopped() {
-	console.log("Sonos Connect stopped");
-	
+sonosObserver.on('Stopped', function(device) {
+	console.log('Sonos Connect stopped');
+
 	avr.setSoundModeToAuto(function() {
 		avr.powerOff();
 	});
-}
-
-function getSonosState(callback) {
-	sonosDevice.getCurrentState(function(err, state) {
-		if (state!=lastState) {
-			
-			switch (state) {
-			case "playing":
-				handleSonosConnectStarted();
-				break;
-			case "stopped":
-				handleSonosConnectStopped();
-				break;
-			}
-			
-			lastState = state;
-		}
-		
-		if (callback)
-			callback();
-	});
-}
-
-function query_getSonosState() {
-	getSonosState(function() {
-		setTimeout(query_getSonosState, 1000);
-	});
-}
+});
