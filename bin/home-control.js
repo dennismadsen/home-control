e235dc27bb77f5433ebe5a652b0546e79bf0db56
Dennis Madsen <dennis@hrmadsen.dk>
2015-09-06 00:12:27 +0200

    Handling source switch back
    If Denon is powered on before Sonos is starting, it will not switch
    back to this source, when is Sonos is turned off again.

--- bin/home-control.js
+++ bin/home-control.js
@@ -19,14 +19,11 @@ var sonosDevice;
 var denonSourceSonosConnect = 'AUX1';
 var denonSourceTV = 'TV';
 
-var ignoreAvrSourceChanged = false;
+var sonosConnected = false;
+var denonSourceBeforeSonosWasTurnedOn;
 
-avr.on('SourceChanged', function (newSource) {
-    if (ignoreAvrSourceChanged) {
-        return;
-    }
-        
-    if (newSource === denonSourceTV) {
+avr.on('SourceChanged', function (newSource) {        
+    if (sonosConnected && newSource === denonSourceTV) {
         console.log('Denon AVR changed source to ' + newSource + '. Stopping Sonos.');
         sonosDevice.stop(function (err, stopped) {});
     }
@@ -41,47 +38,64 @@ sonosObserver.on('DeviceAvailable', function (device, attrs) {
 sonosObserver.on('Started', function (device, attrs) {
     console.log('Sonos Connect started');
 
-    turnDenonOnAndSetSourceToSonos();
+    turnDenonOnAndSetSourceToSonos(function() {
+        sonosConnected = true;
+    });
 });
 
 sonosObserver.on('Stopped', function (device, attrs) {
     console.log('Sonos Connect stopped');
 
-    //TODO: Switch Denon source to TV if it was in this state before Sonos started
-
-    denonDisconnectAndOffIfSourceIsSonos();
+    handleSonosStopped();
 });
 
 sonosObserver.on('Paused', function (device, attrs) {
     console.log('Sonos Connect paused');
-
-    denonDisconnectAndOffIfSourceIsSonos();
+    
+    handleSonosStopped();
 });
 
-function turnDenonOnAndSetSourceToSonos() {
+function handleSonosStopped() {
+    if (denonSourceBeforeSonosWasTurnedOn !== undefined) {
+        avr.setSource(denonSourceBeforeSonosWasTurnedOn);
+        denonSourceBeforeSonosWasTurnedOn = undefined;
+    } else {
+        disconnectDenonAndTurnOffIfSourceIsSonos();
+    }
+    sonosConnected = false;
+}
+
+function turnDenonOnAndSetSourceToSonos(successCallback) {
     avr.connect();
     
-    ignoreAvrSourceChanged = true;
-
-    var endFunction = function(result) {
-        console.log('endFunctionCalled: '+result);
-        ignoreAvrSourceChanged = false;
-    };
-
-    console.log('Turning Denon on');
-    avr.powerOn(function (result) {
-        console.log('powered on');
-        avr.setSource(denonSourceSonosConnect, function(result) {
-            console.log('source set');
-            ignoreAvrSourceChanged = false;
-        }, endFunction);
-    }, endFunction);
+    avr.getPowerState(function (result) {
+        if (result === 'ON') {
+            avr.getSource(function (result) {
+                denonSourceBeforeSonosWasTurnedOn = result;
+                avr.setSource(denonSourceSonosConnect, function (result) {
+                    if (successCallback) {
+                        successCallback();
+                    }
+                });
+            });
+        } else {
+                     
+            avr.powerOn(function (result) {
+                avr.setSource(denonSourceSonosConnect, function(result) {
+                    if (successCallback) {
+                        successCallback();
+                    }
+                });
+            });
+            
+        }    
+    });
     
     // TODO: do we need to change sound mode?
     //avr.setSoundMode('MCH STEREO');
 }
 
-function denonDisconnectAndOffIfSourceIsSonos() {
+function disconnectDenonAndTurnOffIfSourceIsSonos() {
     avr.getSource(function (result) {
         if (result === denonSourceSonosConnect) {
             console.log('Powering Denon off');
